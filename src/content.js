d9ccc9fe23a03ed222adc686417cef54a06233dc
Gert Mertes <hey@gertmertes.be>
2021-10-18 01:43:16 +0800

    First version of skip logic

--- src/content.js
+++ src/content.js
@@ -1,6 +1,33 @@
 const console = require('./console');
-console.log('Version ' + VERSION)
 
-window.addEventListener('load', () => {
+(() => {
+    window.addEventListener('load', () => {
+        console.log('Version ' + VERSION)
+        bodyObserver.observe(document.body, observerOptions);
+    });
+})();
 
+const bodyObserver = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+        mutation.addedNodes.forEach(node => {
+            try{
+                if(node instanceof HTMLElement){
+                    const skipButton = node.querySelector('button[data-uia="player-skip-intro"]');
+                    if(skipButton){
+                        console.log('Skip button seen, clicking...');
+                        skipButton.style.visible = true;
+                        skipButton.querySelector('.ltr-18i00qw').innerHTML = "Skipping...";
+                        setTimeout(skipButton.click(), 1000);
+                    }
+                }
+            }catch (error){
+                console.warn(error);
+            }
+        });
+    });
 });
+const observerOptions = {
+    subtree: true,
+    childList: true
+};
+
