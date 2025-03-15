c697e038e35bf96c95b08cc0733c823c41f8415a
Gert Mertes <hey@gertmertes.be>
2025-03-15 23:22:07 +0000

    Manifest v3

--- src/content.js
+++ src/content.js
@@ -1,11 +1,9 @@
 const console = require('./console');
 
-(() => {
-    window.addEventListener('load', () => {
-        console.log('Version', VERSION);
-        bodyObserver.observe(document.body, observerOptions);
-    });
-})();
+window.addEventListener('load', () => {
+    console.log('Version', VERSION);
+    bodyObserver.observe(document.body, observerOptions);
+});
 
 const observerOptions = {
     subtree: true,
@@ -15,24 +13,25 @@ const observerOptions = {
 const bodyObserver = new MutationObserver(mutations => {
     mutations.forEach(mutation => {
         mutation.addedNodes.forEach(node => {
-                if(node instanceof HTMLElement){
-                    const skipButton = node.querySelector('button[data-uia="player-skip-intro"]');
-                    if(skipButton){
-                        console.log('Skip button seen, clicking..');
-                        // small delay just for visuals
-                        setTimeout(function() {
-                            try{
-                                skipButton.click();
-                            }catch (error) {
-                                console.warn(error);
-                            }}, 800);
-                        try{
-                            skipButton.querySelector('span').innerHTML = "Skipping...";
-                        }catch (error) {
-                            console.warn(error);
-                        }
-                    }
+            if (!(node instanceof HTMLElement)) return;
+
+            const skipButton = node.querySelector('button[data-uia="player-skip-intro"]');
+            if (!skipButton) return;
+
+            console.log('Skip button seen, clicking...');
+            // small delay just for visuals
+            setTimeout(function () {
+                try {
+                    skipButton.click();
+                } catch (error) {
+                    console.warn(error);
                 }
+            }, 800);
+            try {
+                skipButton.querySelector('span').innerHTML = "Skipping...";
+            } catch (error) {
+                console.warn(error);
+            }
         });
     });
 });
